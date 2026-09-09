547ecda966a4c2358a708262229c7910e0943838
itzzerotrace <bwalding.work@icloud.com>
2026-09-09 20:25:42 +0000

    Improve leaderboard page mobile layout

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -2,54 +2,32 @@ import DashboardShell from "@/components/layout/DashboardShell";
 import LeaderboardHeader from "@/components/leaderboard/LeaderboardHeader";
 import EmployeeRankingCard from "@/components/leaderboard/EmployeeRankingCard";
 
+export default function Leaderboard() {
+  return (
+    <DashboardShell>
+      <div className="space-y-6 sm:space-y-8">
+        <LeaderboardHeader />
 
-export default function Leaderboard(){
+        <section className="space-y-3 sm:space-y-4">
+          <EmployeeRankingCard
+            rank={1}
+            name="Bryce"
+            score="92%"
+          />
 
-return (
+          <EmployeeRankingCard
+            rank={2}
+            name="Sarah"
+            score="88%"
+          />
 
-<DashboardShell>
-
-
-<div className="space-y-8">
-
-
-<LeaderboardHeader/>
-
-
-<div className="
-space-y-4
-">
-
-
-<EmployeeRankingCard
-rank={1}
-name="Bryce"
-score="92%"
-/>
-
-
-<EmployeeRankingCard
-rank={2}
-name="Sarah"
-score="88%"
-/>
-
-
-<EmployeeRankingCard
-rank={3}
-name="Mike"
-score="82%"
-/>
-
-
-</div>
-
-
-</div>
-
-
-</DashboardShell>
-
-)
-
+          <EmployeeRankingCard
+            rank={3}
+            name="Mike"
+            score="82%"
+          />
+        </section>
+      </div>
+    </DashboardShell>
+  );
 }
